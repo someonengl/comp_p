aba1237a17bb609be8c421575c76a42d6a66ccd3
someonengl <onlinecasur67@gmail.com>
2026-04-20 20:20:30 +0400

    Build at 2026-04-20 20:20:29

--- code.cpp
+++ code.cpp
@@ -5,38 +5,27 @@ using namespace std;
 #define all(v) v.begin(),v.end()
 bool testcases=0;
 const int MOD=1e9;
+int vis[1000001];
+vector<int> v;
 void solve(){
-    int d,k,t;
-    cin>>k>>d>>t;
-    t=4*t;
-    d*=2;
-    k*=2;
-    d=((k+d-1)/d)*d;
-    int l=0,r=4e18,mid,ans=-1;
-    while (l<=r) {
-        mid=(l+r)/2; // checkung123131
-        int num=mid;
-        num=(mid/d)*(2*k+(d-k));
-        int rem=mid%d;
-        if (rem>k)rem=(2*(k)+(rem-k));
-        else rem=2*rem;
-        num+=rem;
-        if (num>=t) {
-            ans=mid;
-            r=mid-1;
-        }
-        else l=mid+1;
-    }
-    //cout<<ans<<endl;
-    double anss=ans;
-    anss/=2;
-    cout.precision(1);
-    cout<<fixed<<anss<<endl;
+    int n;
+    cin>>n;
+    cout<<*(--upper_bound(all(v),n))<<endl;
 }
 signed main() {
     iostream::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+    for (int i=2;i<=1e6;i++) {
+        if (vis[i]==0) {
+            for (int j=i+i;j<=1e6;j++) {
+                vis[j]++;
+            }
+        }
+    }
+    for (int i=1;i<=1e6;i++) {
+        if (vis[i]==2)v.push_back(i*i);
+    }
     int t=1;
     if (testcases)cin>>t;
     for(int i=1;i<=t;i++){
