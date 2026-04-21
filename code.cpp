df0d0266acd0db2a8322a489d4014c69c417a3dd
someonengl <onlinecasur67@gmail.com>
2026-04-21 09:35:00 +0400

    Build at 2026-04-21 09:35:00

--- code.cpp
+++ code.cpp
@@ -5,31 +5,46 @@ using namespace std;
 #define all(v) v.begin(),v.end()
 bool testcases=0;
 const int MOD=1e9;
-map<int,array<int,2>>mp;
+vector<int> ans;
+vector<int> g[200005];
+bool vis[200005];
+bool flag=1;
+int par[200005];
+void dfs(int start,int p) {
+    par[start]=p;
+    vis[start]=1;
+    ans.push_back(start);
+    vector<int> adj;
+    for (int i:g[start]) {
+        if (!vis[i]) {
+            adj.push_back(i);
+        }
+    }
+    if (start==1) {
+        if (flag)flag=0;
+        else if (adj.size()==0) {
+            for (int i:ans) {
+                cout<<i<<' ';
+            }
+            exit(0);
+        }
+    }
+    for (int i:adj)dfs(i,start);
+    if (adj.size()==0)dfs(p,par[p]);
+}
 void solve(){
-    int h,w,n;
-    cin>>h>>w>>n;
-    vector<array<int,2>> v[2];
-    for (int i=1;i<=n;i++) {
+    int n;
+    cin>>n;
+    memset(par,-1,sizeof(par));
+    for (int i=1;i<n;i++) {
         int l,r;
         cin>>l>>r;
-        v[0].push_back({l,i});
-        v[1].push_back({r,i});
+        g[l].push_back(r);
+        g[r].push_back(l);
     }
-    sort(all(v[0]));
-    sort(all(v[1]));
-    for (int f=0;f<2;f++) {
-        int c=1;
-        for (auto& [num,ind]:v[f]) {
-            num=c++;
-        }
-    }
-    for (int f=0;f<2;f++) {
-        for (auto& [num,ind]:v[f]) {
-            mp[ind][f]=num;
-        }
-    }
-    for (int i=1;i<=n;i++)cout<<mp[i][0]<<' '<<mp[i][1]<<endl;
+    for (int i=1;i<=n;i++)sort(all(g[i]));
+    dfs(1,-1);
+    for (int i:ans)cout<<i<<' ';
 }
 signed main() {
     iostream::sync_with_stdio(0);
