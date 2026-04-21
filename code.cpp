387d8bea46fc7030abfe12da3a1a107b6ea27bcc
someonengl <onlinecasur67@gmail.com>
2026-04-21 08:14:20 +0400

    Build at 2026-04-21 08:14:20

--- code.cpp
+++ code.cpp
@@ -3,14 +3,21 @@ using namespace std;
 #define int long long
 #define endl '\n'
 #define all(v) v.begin(),v.end()
-bool testcases=1;
+bool testcases=0;
 const int MOD=1e9;
-int vis[1000001];
-vector<int> v;
 void solve(){
-    int n;
-    cin>>n;
-    cout<<((int)sqrtl(n/2))+((int)sqrtl(n/4))<<endl;
+    int n,m;
+    cin>>n>>m;
+    int v[n+1];
+    for (int i=1;i<=n;i++)cin>>v[i];
+    int lcmm=1;
+    for (int i=1;i<=n;i++)lcmm=lcm(lcmm,v[i]);
+    vector<int> ans;
+    for (int i=1;i<=m;i++) {
+        if (gcd(i,lcmm)==1)ans.push_back(i);
+    }
+    cout<<ans.size()<<endl;
+    for (int i:ans)cout<<i<<endl;
 }
 signed main() {
     iostream::sync_with_stdio(0);
