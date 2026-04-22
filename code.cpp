f283c41556ee9d0f1573c090f3539d7442956539
someonengl <onlinecasur67@gmail.com>
2026-04-22 20:51:59 +0400

    Build at 2026-04-22 20:51:59

--- code.cpp
+++ code.cpp
@@ -8,16 +8,34 @@ void solve(){
     int n,m;
     cin>>n>>m;
     cout<<m+n/2<<endl;
-    int ans[n+1][m+1];
-    memset(ans,0,sizeof(ans));
+    int v[n+1][m+1];
+    memset(v,0,sizeof(v));
     for (int i=1;i<=n/2-1+n%2;i++) {
         for (int j=1;j<=(m+1)/2+1-m%2;j++) {
-            ans[i][j]=1;
+            v[i][j]=1;
         }
     }
+    int A=0,B=0;
+    for (int i=1;i<=n;i++) {
+        int c1=0,c2=0;
+        for (int u=1;u<=m;u++) {
+            if (v[i][u]==1)c1++;
+            else c2++;
+        }
+        if (c1>c2)B++;
+    }
+    for (int u=1;u<=m;u++) {
+        int c1=0,c2=0;
+        for (int i=1;i<=n;i++) {
+            if (v[i][u]==1)c1++;
+            else c2++;
+        }
+        if (c2>c1)A++;
+    }
+    cout<<A+B<<endl;
     for (int i=1;i<=n;i++) {
         for (int j=1;j<=m;j++) {
-            cout<<(ans[i][j]?'+':'-');
+            cout<<(v[i][j]?'+':'-');
         }
         cout<<endl;
     }
