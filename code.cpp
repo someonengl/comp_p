9bbe74848cea857bad6beeeafbabdc672fb05529
someonengl <onlinecasur67@gmail.com>
2026-04-22 20:43:51 +0400

    Build at 2026-04-22 20:43:51

--- code.cpp
+++ code.cpp
@@ -17,7 +17,7 @@ void solve(){
     }
     for (int i=1;i<=n;i++) {
         for (int j=1;j<=m;j++) {
-            cout<<ans[i][j]?'+':'-';
+            cout<<(ans[i][j]?'+':'-');
         }
         cout<<endl;
     }
