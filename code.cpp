1333b578a61148165573ddbcd3368f05de278a0b
someonengl <onlinecasur67@gmail.com>
2026-04-22 20:59:21 +0400

    Build at 2026-04-22 20:59:21

--- code.cpp
+++ code.cpp
@@ -4,40 +4,30 @@ using namespace std;
 //#define endl '\n'
 #define all(v) v.begin(),v.end()
 bool testcases=1;
-void solve(){
-    int n,m;
-    cin>>n>>m;
-    int v[n+1][m+1];
-    memset(v,0,sizeof(v));
-    for (int i=1;i<=n/2-1+n%2;i++) {
-        for (int j=1;j<=(m+1)/2+1-m%2;j++) {
-            v[i][j]=1;
+void solve() {
+    int n;
+    cin>>n;
+    int v[n];
+    int ans=0;
+    for (int i=0;i<n;i++)cin>>v[i];
+    for (int mask=0;mask<(1<<n);mask++) {
+        int temp=0;
+        int sum=0,ls=0;
+        for (int i=0;i<=n;i++) {
+            sum+=v[i];
+            if ((mask>>i)&1) {
+                if (ls>sum) {
+                    temp=-999999999;
+                    break;
+                }
+                temp++;
+                ls=sum;
+                sum=0;
+            }
         }
+        ans=max(ans,temp);
     }
-    int A=0,B=0;
-    for (int i=1;i<=n;i++) {
-        int c1=0,c2=0;
-        for (int u=1;u<=m;u++) {
-            if (v[i][u]==1)c1++;
-            else c2++;
-        }
-        if (c1>c2)B++;
-    }
-    for (int u=1;u<=m;u++) {
-        int c1=0,c2=0;
-        for (int i=1;i<=n;i++) {
-            if (v[i][u]==1)c1++;
-            else c2++;
-        }
-        if (c2>c1)A++;
-    }
-    cout<<A+B<<endl;
-    for (int i=1;i<=n;i++) {
-        for (int j=1;j<=m;j++) {
-            cout<<(v[i][j]?'+':'-');
-        }
-        cout<<endl;
-    }
+    cout<<ans<<endl;
 }
 signed main() {
     iostream::sync_with_stdio(0);
