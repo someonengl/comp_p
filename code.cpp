052177413ac93366119fd0385c5b173ccd98221e
someonengl <onlinecasur67@gmail.com>
2026-04-23 09:24:06 +0400

    Build at 2026-04-23 09:24:06

--- code.cpp
+++ code.cpp
@@ -4,29 +4,8 @@ using namespace std;
 //#define endl '\n'
 #define all(v) v.begin(),v.end()
 bool testcases=0;
-int n,m;
-int calc(vector<vector<char>>& v) { // c1 red c2 blue
-    int A=0,B=0;
-    int a=n,b=m;
-    for (int i=1;i<=a;i++) {
-        int c1=0,c2=0;
-        for (int u=1;u<=b;u++) {
-            if (v[i][u]=='+')c1++;
-            else c2++;
-        }
-        if (c1>c2)B++;
-    }
-    for (int u=1;u<=b;u++) {
-        int c1=0,c2=0;
-        for (int i=1;i<=a;i++) {
-            if (v[i][u]=='+')c1++;
-            else c2++;
-        }
-        if (c2>c1)A++;
-    }
-    return A+B;
-}
 void solve() {
+    int n,m;
     cin>>n>>m;
     bool flag=0;
     vector<vector<char>> ans(n+1,vector<char>(m+1,'-'));
@@ -37,6 +16,27 @@ void solve() {
         }
     }
     afterloop:
+    function<int(vector<vector<char>>&)> calc=[&](vector<vector<char>>& v) { // c1 red c2 blue
+        int A=0,B=0;
+        int a=n,b=m;
+        for (int i=1;i<=a;i++) {
+            int c1=0,c2=0;
+            for (int u=1;u<=b;u++) {
+                if (v[i][u]=='+')c1++;
+                else c2++;
+            }
+            if (c1>c2)B++;
+        }
+        for (int u=1;u<=b;u++) {
+            int c1=0,c2=0;
+            for (int i=1;i<=a;i++) {
+                if (v[i][u]=='+')c1++;
+                else c2++;
+            }
+            if (c2>c1)A++;
+        }
+        return A+B;
+    };
     cout<<calc(ans)<<endl;
     for (int i=1;i<=n;i++) {
         for (int j=1;j<=m;j++) {
