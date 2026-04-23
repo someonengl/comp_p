e48e43119cd44cdedc3ba7a7503704a0d0705162
someonengl <onlinecasur67@gmail.com>
2026-04-23 09:25:33 +0400

    Build at 2026-04-23 09:25:33

--- code.cpp
+++ code.cpp
@@ -16,28 +16,28 @@ void solve() {
     //     }
     // }
     // afterloop:
-    // function<int(vector<vector<char>>&)> calc=[&](vector<vector<char>>& v) { // c1 red c2 blue
-    //     int A=0,B=0;
-    //     int a=n,b=m;
-    //     for (int i=1;i<=a;i++) {
-    //         int c1=0,c2=0;
-    //         for (int u=1;u<=b;u++) {
-    //             if (v[i][u]=='+')c1++;
-    //             else c2++;
-    //         }
-    //         if (c1>c2)B++;
-    //     }
-    //     for (int u=1;u<=b;u++) {
-    //         int c1=0,c2=0;
-    //         for (int i=1;i<=a;i++) {
-    //             if (v[i][u]=='+')c1++;
-    //             else c2++;
-    //         }
-    //         if (c2>c1)A++;
-    //     }
-    //     return A+B;
-    // };
-    // cout<<calc(ans)<<endl;
+    function<int(vector<vector<char>>&)> calc=[&](vector<vector<char>>& v) { // c1 red c2 blue
+        int A=0,B=0;
+        int a=n,b=m;
+        for (int i=1;i<=a;i++) {
+            int c1=0,c2=0;
+            for (int u=1;u<=b;u++) {
+                if (v[i][u]=='+')c1++;
+                else c2++;
+            }
+            if (c1>c2)B++;
+        }
+        for (int u=1;u<=b;u++) {
+            int c1=0,c2=0;
+            for (int i=1;i<=a;i++) {
+                if (v[i][u]=='+')c1++;
+                else c2++;
+            }
+            if (c2>c1)A++;
+        }
+        return A+B;
+    };
+    cout<<calc(ans)<<endl;
     for (int i=1;i<=n;i++) {
         for (int j=1;j<=m;j++) {
             cout<<ans[i][j];
