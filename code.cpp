b87aac0ef517473fce6006f50d8caba57fb32192
someonengl <onlinecasur67@gmail.com>
2026-04-23 09:19:17 +0400

    Build at 2026-04-23 09:19:17

--- code.cpp
+++ code.cpp
@@ -4,30 +4,44 @@ using namespace std;
 //#define endl '\n'
 #define all(v) v.begin(),v.end()
 bool testcases=0;
-void solve() {
-    int n;
-    cin>>n;
-    int v[n];
-    int ans=0;
-    for (int i=0;i<n;i++)cin>>v[i];
-    for (int mask=0;mask<(1<<n);mask++) {
-        int temp=0;
-        int sum=0,ls=0;
-        for (int i=0;i<=n;i++) {
-            sum+=v[i];
-            if ((mask>>i)&1) {
-                if (ls>sum) {
-                    temp=-999999999;
-                    break;
-                }
-                temp++;
-                ls=sum;
-                sum=0;
-            }
+int n,m;
+int calc(vector<vector<char>>& v) { // c1 red c2 blue
+    int A=0,B=0;
+    int a=n,b=m;
+    for (int i=1;i<=a;i++) {
+        int c1=0,c2=0;
+        for (int u=1;u<=b;u++) {
+            if (v[i][u]=='+')c1++;
+            else c2++;
         }
-        ans=max(ans,temp);
+        if (c1>c2)B++;
     }
-    cout<<ans<<endl;
+    for (int u=1;u<=b;u++) {
+        int c1=0,c2=0;
+        for (int i=1;i<=a;i++) {
+            if (v[i][u]=='+')c1++;
+            else c2++;
+        }
+        if (c2>c1)A++;
+    }
+    return A+B;
+}
+void solve() {
+    cin>>n>>m;
+    bool flag=0;
+    vector<vector<char>> ans(n+1,vector<char>(m+1,'-'));
+    for (int i=1;i<=(n-1)/2;i++) {
+        for (int j=1;j<=(m+1)/2+1;j++) {
+            ans[i][j]='+';
+        }
+    }
+    cout<<calc(ans)<<endl;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
+            cout<<ans[i][j];
+        }
+        cout<<endl;
+    }
 }
 signed main() {
     iostream::sync_with_stdio(0);
