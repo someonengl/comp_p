b23e3b836b9bfdc751c282067e9ee73e80dc1746
someonengl <onlinecasur67@gmail.com>
2026-04-23 09:50:35 +0400

    Build at 2026-04-23 09:50:35

--- code.cpp
+++ code.cpp
@@ -23,11 +23,36 @@ void solve() {
                 cc[j]++;
                 cr[i]++;
             }
+        }
+    }
+    function<int(vector<vector<char>>&)> calc=[&](vector<vector<char>>& v) { // c1 red c2 blue
+        int A=0,B=0;
+        int a=n,b=m;
+        for (int i=1;i<=a;i++) {
+            int c1=0,c2=0;
+            for (int u=1;u<=b;u++) {
+                if (v[i][u]=='+')c1++;
+                else c2++;
+            }
+            if ((c1>c2))B++;
+        }
+        for (int u=1;u<=b;u++) {
+            int c1=0,c2=0;
+            for (int i=1;i<=a;i++) {
+                if (v[i][u]=='+')c1++;
+                else c2++;
+            }
+            if (c2>c1)A++;
+        }
+        return A+B;
+    };
+    cout<<calc(v)<<endl;
+    for (int i=1;i<=n;i++) {
+        for (int j=1;j<=m;j++) {
             cout<<v[i][j];
         }
         cout<<endl;
     }
-
 }
 signed main() {
     iostream::sync_with_stdio(0);
