d39596bf7a65fefc8905b73a637e57418b30afeb
someonengl <onlinecasur67@gmail.com>
2026-04-23 09:54:52 +0400

    Build at 2026-04-23 09:54:52

--- code.cpp
+++ code.cpp
@@ -25,31 +25,18 @@ void solve() {
             }
         }
     }
-    function<int(vector<vector<char>>&)> calc=[&](vector<vector<char>>& v) { // c1 red c2 blue
-        int A=0,B=0;
-        int a=n,b=m;
-        for (int i=1;i<=a;i++) {
-            int c1=0,c2=0;
-            for (int u=1;u<=b;u++) {
-                if (v[i][u]=='+')c1++;
-                else c2++;
-            }
-            if ((c1>c2))B++;
-        }
-        for (int u=1;u<=b;u++) {
-            int c1=0,c2=0;
-            for (int i=1;i<=a;i++) {
-                if (v[i][u]=='+')c1++;
-                else c2++;
-            }
-            if (c2>c1)A++;
-        }
-        return A+B;
-    };
-    cout<<calc(v)<<endl;
+    int ans=0;
+    for (int i=1;i<=n;i++) {
+        if (cr[i]>=m/2+1)ans++;
+    }
+    for (int j=1;j<=m;j++) {
+        if (cc[j]<=(n-1)/2)ans++;
+    }
+    cout<<ans<<endl;
+    if (flag)swap(n,m);
     for (int i=1;i<=n;i++) {
         for (int j=1;j<=m;j++) {
-            cout<<v[i][j];
+            cout<<(flag?(v[i][j]=='+'?'-':'+'):v[i][j]);
         }
         cout<<endl;
     }
