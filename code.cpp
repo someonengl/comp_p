58b516a5d21bc97302c0daf3d96d3d3467f20625
someonengl <onlinecasur67@gmail.com>
2026-04-23 09:47:38 +0400

    Build at 2026-04-23 09:47:38

--- code.cpp
+++ code.cpp
@@ -8,52 +8,26 @@ void solve() {
     int n,m;
     cin>>n>>m;
     bool flag=0;
-    if (n>m){swap(n,m);flag=1;}
-    vector<vector<char>> ans(n+1,vector<char>(m+1,'-'));
-    if (n<3)goto afterloop;
-    for (int i=1;i<=(n-1)/2;i++) {
-        for (int j=1;j<=(m)/2+1;j++) {
-            ans[i][j]='+';
-        }
+    if (n>m) {
+        swap(n,m);
+        flag=1;
     }
-    afterloop:
-    function<int(vector<vector<char>>&)> calc=[&](vector<vector<char>>& v) { // c1 red c2 blue
-        int A=0,B=0;
-        int a=n,b=m;
-        for (int i=1;i<=a;i++) {
-            int c1=0,c2=0;
-            for (int u=1;u<=b;u++) {
-                if (v[i][u]=='+')c1++;
-                else c2++;
-            }
-            if ((c1>c2))B++;
-        }
-        for (int u=1;u<=b;u++) {
-            int c1=0,c2=0;
-            for (int i=1;i<=a;i++) {
-                if (v[i][u]=='+')c1++;
-                else c2++;
-            }
-            if (c2>c1)A++;
-        }
-        return A+B;
-    };
-    cout<<calc(ans)<<endl;
-    if (flag) {
-        for (int j=1;j<=m;j++) {
-            for (int i=1;i<=n;i++) {
-                cout<<(ans[i][j]=='+'?'-':'+');
-            }
-            cout<<endl;
-        }
-        return;
-    }
+    int cc[m+1],cr[n+1];
+    memset(cc,0,sizeof(cc));
+    memset(cr,0,sizeof(cr));
+    vector<vector<char>> v(n+1,vector<char>(m+1,'-'));
     for (int i=1;i<=n;i++) {
         for (int j=1;j<=m;j++) {
-            cout<<ans[i][j];
+            if (cc[j]<(n-1)/2 and cr[i]<(m/2+1)) {
+                v[i][j]='+';
+                cc[j]++;
+                cr[i]++;
+            }
+            cout<<v[i][j];
         }
         cout<<endl;
     }
+
 }
 signed main() {
     iostream::sync_with_stdio(0);
